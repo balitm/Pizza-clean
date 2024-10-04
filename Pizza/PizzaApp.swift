b4155675418498b21483d5ea9556aec60d1fa451
Balázs Kilvády <kilvady.balazs@arteries.hu>
2024-10-04 18:36:19 +0200

    Initial Commit

---
+++ Pizza/PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaApp.swift
+//  Pizza
+//
+//  Created by Balázs Kilvády on 2024. 10. 04..
+//
+
+import SwiftUI
+
+@main
+struct PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
