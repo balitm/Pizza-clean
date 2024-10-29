07e28f76090f6d7f63827508543feed8747713af
Balázs Kilvády <kilvady.balazs@arteries.hu>
2024-10-29 14:29:56 +0100

    Save cart on going to background.

--- Pizza/ContentView.swift
+++ Pizza/ContentView.swift
@@ -6,11 +6,23 @@
 //
 
 import SwiftUI
+import Domain
+import Factory
 
 struct ContentView: View {
+    @Environment(\.scenePhase) var scenePhase
+    @State private var saveService = Container.shared.saveUseCase()
+
     var body: some View {
         AlertHelperView {
             MenuListView()
+        }
+        .onChange(of: scenePhase) { phase in
+            if phase == .background {
+                Task {
+                    try? await saveService.saveCart()
+                }
+            }
         }
     }
 }
@@ -18,5 +30,6 @@ struct ContentView: View {
 #if DEBUG
 #Preview {
     ContentView()
+        .environmentObject(AlertHelper())
 }
 #endif
