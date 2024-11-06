97644163ecd53b41bd6ea30d17a6ecc15d8e1d13
Balázs Kilvády <kilvady.balazs@arteries.hu>
2024-11-06 18:25:26 +0100

    Fix accent color.

--- Pizza/PizzaApp.swift
+++ Pizza/PizzaApp.swift
@@ -16,12 +16,22 @@ struct PizzaApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                // .tint(.accent)
                 .environmentObject(AlertHelper())
         }
     }
 
     private func configNavBar() {
-        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.accent]
-        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.accent]
+        // Using UIColor.accent breaks the global accent color in SwiftUI
+        var aColor: UIColor {
+            UIColor { traits -> UIColor in
+                // Return one of two colors depending on light or dark mode
+                return traits.userInterfaceStyle == .dark ?
+                    UIColor(red: 0.812, green: 0.322, blue: 0.294, alpha: 1) :
+                    UIColor(red: 0.882, green: 0.302, blue: 0.271, alpha: 1)
+            }
+        }
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: aColor]
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: aColor]
     }
 }
